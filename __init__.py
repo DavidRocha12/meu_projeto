d1bfa03236553baf2011a31788f4185a5b0c17ec
DavidRocha <davidedisondarocha1202@gmail.com>
2024-06-23 20:17:10 -0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,34 +1,52 @@
-def adicionar_produto(nome_arquivo, conteudo):
-	"""Vai receber nome do arquivo, e vai receber nome
-	do objeto para ser adicionado"""
-	arquivo = open(nome_arquivo, "a")
-	arquivo.write(f"{conteudo}\n")
-	arquivo.close()
+from main_configuration import *
 
 
-def leitura_dados(arquivo):
-	arquivo = open(arquivo, "r")
-	listas = []
-	for produtos in arquivo:
-		listas.append(produtos.strip())
-	arquivo.close()
-	return listas
+def opcao_1(nome_item, nome_valor_item):
+	while True:
+		produto = input("Digite o nome do produto: ")
+		print()
+
+		valor_produto = input("Digite o valor: ")
+		valor_produto = float(valor_produto)
+
+		cont = input("Quer continuar adicionando? [S/N] ")
+		while cont not in "SsNn":
+			cont = input("Quer continuar adicionando? [S/N] ")
+
+		adicionando = adicionar_produto(nome_item, produto)
+		adicionando_valor = adicionar_produto(nome_valor_item, valor_produto)
+
+		print()
+		if cont in "Nn":
+			break
 
 
-def deletar_item(arquivo, indice):
-	arquivo = open(arquivo, "r")
-	listas = []
-	for produtos in arquivo:
-		listas.append(produtos.strip())
-	arquivo.close()
-	remover = indice
-	listas.pop(int(remover))
-	return listas
+def opcao_2(nome_item, nome_valor_item):
+
+	conta = "Contas a Pagar" 
+	print(f"{conta:*^40}")
+	print()
+
+	dados_arquivo = leitura_dados(nome_item)
+	dados_arquivo_item = leitura_dados(nome_valor_item)
+
+	soma = 0
+	for chave, valor in enumerate(dados_arquivo):
+		print(f"{chave + 1} {dados_arquivo[chave]:.<28}{float(dados_arquivo_item[chave]):.2f}")
+		soma += float(dados_arquivo_item[chave])
+
+	print()
+	print(f"Saldo total a pagar: R$ {soma:.2f}")
+	print("*" * 40)
+	print()
 
 
-def atualizar_dados(arquivo, item):
-	atualizar_arquivo = open(arquivo, "w")
-	atualizar_arquivo.close()
-	for produto in item:
-		atualizado = adicionar_produto(arquivo, produto)
+def opcao_3(nome_item, nome_valor_item):
+	indice = input("Digite o número do produto que deseja ser deletado: ")
+	indice = int(indice)
 
+	deletar_dados = deletar_item(nome_item, indice-1)
+	deletar_valor = deletar_item(nome_valor_item, indice-1)
+
+	dados_atualizados = atualizar_dados(nome_item, deletar_dados)
+	dados_atualizados_2 = atualizar_dados(nome_valor_item, deletar_valor)
